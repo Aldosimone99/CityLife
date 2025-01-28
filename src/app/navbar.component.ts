ffa190a3e9de15cdef4fa43cfdec1c84ee5816c5
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-01-28 19:26:03 +0100

    localstorage implemented

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -1,9 +1,19 @@
+// filepath: /Users/aldosimone/Documents/GitHub/CityLife/src/app/navbar/navbar.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../app/services/local-storage.service';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    standalone: false,
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: false,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private router: Router, private localStorageService: LocalStorageService) {}
+
+  logout() {
+    this.localStorageService.removeItem('token'); // Rimuove il token usando il servizio LocalStorageService
+    this.router.navigate(['/login']); // Reindirizza alla pagina di login
+  }
+}
