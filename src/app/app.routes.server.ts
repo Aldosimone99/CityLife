ced77461560362e1ff4c1de5c9a4ca7f8c2a2748
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-02-08 17:15:34 +0100

    fixed prerender error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'user/:id',
+    renderMode: RenderMode.Server // Imposta un renderMode diverso per non usare il prerendering
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
