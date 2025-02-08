2c80363b5e7cc4ff899ad02f50c516b56a92ebf9
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-02-08 17:47:43 +0100

    fixed mobile layout

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'user/:id',
-    renderMode: RenderMode.Server // Imposta un renderMode diverso per non usare il prerendering
+    renderMode: RenderMode.Server
   },
   {
     path: '**',
